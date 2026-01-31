2d8da71baf7cb8041561bbe73482b151377a1199
nicolaoum <nicolaoum@coventry.ac.uk>
2026-01-31 16:37:11 +0000

    Restore standalone output for Amplify SSR

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Removed 'standalone' output for Amplify compatibility
+  output: 'standalone',
   distDir: '.next',
 }
 
