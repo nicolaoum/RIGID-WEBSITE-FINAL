840bb166f7ff566148cb847fed82f5ee2d4eb8f9
nicolaoum <nicolaoum@coventry.ac.uk>
2026-02-15 16:54:30 +0000

    Remove auto-refresh

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,18 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import { useSessionRestore } from '@/lib/useSessionRestore'
 
 export default function App({ Component, pageProps }: AppProps) {
   // Automatically restore user session from localStorage on app startup
   useSessionRestore()
 
-  return (
-    <>
-      <Head>
-        <meta httpEquiv="refresh" content="5" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
