30b6f7f979fc57f778d1a4fd9ca9e1fab6e8a4ea
nicolaoum <nicolaoum@coventry.ac.uk>
2026-02-15 16:52:00 +0000

    Add 5-second auto-refresh to all pages

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,10 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { useSessionRestore } from '@/lib/useSessionRestore'
 
 export default function App({ Component, pageProps }: AppProps) {
   // Automatically restore user session from localStorage on app startup
   useSessionRestore()
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="refresh" content="5" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
